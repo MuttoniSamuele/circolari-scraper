52d945d8d2841509f1247771fe0aa3baf52baa67
mutti <imperialvictor15@gmail.com>
2024-02-13 12:55:04 +0100

    Add cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use scraper::{Html, Selector};
+use std::env;
 
 struct Circolare {
     id: String,
@@ -8,13 +9,50 @@ struct Circolare {
 }
 
 fn main() {
-    let Ok(circolari) = scrape_circolari(1) else {
-        println!("Error fetching circolari");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
         return;
-    };
-    for c in circolari {
-        println!("{}\t{}\t{}\t{}", c.id, c.title, c.date, c.link);
     }
+    let res = match args[1].as_str() {
+        "page" => {
+            if args.len() == 3 {
+                if let Ok(p) = args[2].parse() {
+                    page(p)
+                } else {
+                    Err(format!(
+                        "Argument must be an integer, found \"{}\"",
+                        args[2]
+                    ))
+                }
+            } else {
+                Err("Bad arguments".to_string())
+            }
+        }
+        "search" => Ok(()),
+        "help" => Ok(()),
+        cmd => Err(format!("Unknown sub-command \"{}\"", cmd)),
+    };
+    if let Err(err) = res {
+        println!("Error: {}", err);
+    }
+}
+
+fn page(page: u16) -> Result<(), String> {
+    let circolari = scrape_circolari(page).map_err(|e| e.to_string())?;
+    print_circolari(&circolari);
+    Ok(())
+}
+
+fn print_circolari(circolari: &Vec<Circolare>) {
+    for c in circolari {
+        println!("# {} ({})\n{}\n{}\n", c.id, c.date, c.title, c.link);
+    }
+}
+
+fn append_circolari(circolari: &mut Vec<Circolare>, page: u16) -> Result<(), reqwest::Error> {
+    let mut new_circolari = scrape_circolari(page)?;
+    circolari.append(&mut new_circolari);
+    Ok(())
 }
 
 fn scrape_circolari(page: u16) -> Result<Vec<Circolare>, reqwest::Error> {
