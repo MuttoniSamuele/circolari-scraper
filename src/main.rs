62c0084a02aecda860df3cba342eac81b661ef94
mutti <imperialvictor15@gmail.com>
2024-02-13 22:43:55 +0100

    Add page_min and page_max and add some comments

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,13 @@ fn main() {
     }
     let res = match args[1].as_str() {
         "page" => {
-            if args.len() == 3 {
-                if let Ok(p) = args[2].parse() {
-                    page(p)
+            if args.len() == 4 {
+                if let (Ok(p_min), Ok(p_max)) = (args[2].parse(), args[3].parse()) {
+                    print_pages(p_min, p_max)
                 } else {
                     Err(format!(
-                        "Argument must be an integer, found \"{}\"",
-                        args[2]
+                        "Arguments must be integer, found \"{}\" and \"{}\"",
+                        args[2], args[3]
                     ))
                 }
             } else {
@@ -37,27 +37,34 @@ fn main() {
     }
 }
 
-fn page(page: u16) -> Result<(), String> {
-    let circolari = scrape_circolari(page).map_err(|e| e.to_string())?;
-    print_circolari(&circolari);
+/// Stampa tutte le circolari da page_min a page_max
+fn print_pages(page_min: u16, page_max: u16) -> Result<(), String> {
+    for page in page_min..=page_max {
+        let circolari = scrape_circolari(page)?;
+        println!("\n[Page {}]\n", page);
+        print_circolari(&circolari);
+    }
     Ok(())
 }
 
+/// Stampa le circolari specificate a schermo
 fn print_circolari(circolari: &Vec<Circolare>) {
     for c in circolari {
         println!("# {} ({})\n{}\n{}\n", c.id, c.date, c.title, c.link);
     }
 }
 
-fn append_circolari(circolari: &mut Vec<Circolare>, page: u16) -> Result<(), reqwest::Error> {
+/// Aggiunge al vettore specificato le circolari nella pagina specificata
+fn append_circolari(circolari: &mut Vec<Circolare>, page: u16) -> Result<(), String> {
     let mut new_circolari = scrape_circolari(page)?;
     circolari.append(&mut new_circolari);
     Ok(())
 }
 
-fn scrape_circolari(page: u16) -> Result<Vec<Circolare>, reqwest::Error> {
+/// Fa lo scrape della pagina di circolari specificata e restituisce un vettore di circolari
+fn scrape_circolari(page: u16) -> Result<Vec<Circolare>, String> {
     let url = format!("https://www.itispaleocapa.edu.it/circolari/page/{}/", page);
-    let text = fetch_webpage(&url)?;
+    let text = fetch_webpage(&url).map_err(|e| e.to_string())?;
     let html = Html::parse_document(&text);
     let selector = Selector::parse("div.post-box-archive").unwrap();
     let mut circolari: Vec<Circolare> = Vec::new();
@@ -75,6 +82,7 @@ fn scrape_circolari(page: u16) -> Result<Vec<Circolare>, reqwest::Error> {
     Ok(circolari)
 }
 
+/// Manda una richiesta GET all'URL specificato e restituisce il contenuto della pagina
 fn fetch_webpage(url: &str) -> Result<String, reqwest::Error> {
     let res = reqwest::blocking::get(url)?;
     let text = res.text()?;
